1f10a85c1d1eca256668c0837b3d2ee1a10c8dad
Anandra <anandrapratap.singh@cgi.com>
2019-09-05 16:58:56 +0530

    Commit sept 05

---
+++ Model/Comments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core_microservice_backend.Model
+{
+    public class Comments
+    {
+        public int commentID { get; set; }
+        public string text { get; set; }
+
+    }
+}
